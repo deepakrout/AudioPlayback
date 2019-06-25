329a4fcfc3b696e315f37d673e128d52864d4f66
Deepak Rout <deepakrout@gmail.com>
2019-06-24 22:07:08 -0400

    Commit after volume controll and scrub bar

--- app/src/main/java/com/iappstogo/audioplayback/MainActivity.java
+++ app/src/main/java/com/iappstogo/audioplayback/MainActivity.java
@@ -2,13 +2,20 @@ package com.iappstogo.audioplayback;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
+    AudioManager audioManager;
 
     public void startPlaying(View startBtn){
         mediaPlayer.start();
@@ -24,7 +31,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
         mediaPlayer = MediaPlayer.create(this,R.raw.harleydavidson);
+
+        SeekBar volumeControl = findViewById(R.id.volumeSeekBar);
+
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(currentVolume);
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i("SeekBar Changed",Integer.toString(i));
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i,0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        final SeekBar scrubSeekBar = (SeekBar) findViewById(R.id.scrubbingSeekBar);
+
+        scrubSeekBar.setMax(mediaPlayer.getDuration());
+
+        scrubSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i("Seekbar moved", Integer.toString((i)));
+                mediaPlayer.seekTo(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        //Timer to run the scrubber
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                scrubSeekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        },0,300);
 
     }
 }
